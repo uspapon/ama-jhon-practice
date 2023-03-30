c08162dc725d98483adffe563a3d20bfc2abca85
USPapon <papon.fsp@gmail.com>
2023-03-30 22:02:29 +0600

    show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,9 +5,15 @@ const Cart = ({cart}) => {
     console.log("cart view", cart);
     let totalPrice = 0; 
     let totalShipping = 0;
+    let quantity = 0;
+    
     for(const product of cart ){
-        totalPrice = totalPrice + product.price;
+        if(product.quantity === 0){
+            product.quantity = 1;
+        }  // we can write product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice*7/100;
     const grandTotal = totalPrice + totalShipping + tax;
@@ -15,7 +21,7 @@ const Cart = ({cart}) => {
     return (
         <div className='cart'>
            <h3>order summery</h3>
-           <p>Selected Items:  {cart.length}</p>
+           <p>Selected Items:  {quantity}</p>
            <p>Total Price: ${totalPrice} </p> 
            <p>Total Shipping:${totalShipping} </p> 
            <p>Tax:${tax.toFixed(2)}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -13,34 +13,44 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, [])
 
-    useEffect(() => {
+    useEffect(() => { // this function retrive cart data from the local storage also handles any new data that user have decided to add into the cart. 
         // console.log("products",products);
-        const storedCart = getShoppingCart();
-        const savedCart = [];
+        const storedCart = getShoppingCart(); // getting cart info from local storage
+        const savedCart = []; // initialization of a new cart which will include stored product in localStorage as well as if user add any more product any time
 
         // step 1: get id
-        for(const id in storedCart){
+        for(const id in storedCart){ // getting  the id of the product saved in local storage
             // console.log(id);
 
         // step 2: get the product using id
-            const savedLocalProductFindingInProductCatalog = products.find(product => product.id === id )
+            const savedLocalProductFindingInProductCatalog = products.find(product => product.id === id ) // finding product details by id(stored in local storage) from the API fetched array of object
         // step 3: get the product qty of product
-           if(savedLocalProductFindingInProductCatalog){
+           if(savedLocalProductFindingInProductCatalog){ // here the var will find the product as an object from the above products.find operation 
                
-               const quantity = storedCart[id];
+               const quantity = storedCart[id]; // here the product object's quantity field will be updated since user may have selected more than one qty to buy so updating the qty field in the product object transform the product data into a user added product into the cart. 
                savedLocalProductFindingInProductCatalog.quantity = quantity; //here we are updating product quantity into the product catalogue 
         // step 4: add the added product to the saved cart found in local storage
-               savedCart.push(savedLocalProductFindingInProductCatalog);
+               savedCart.push(savedLocalProductFindingInProductCatalog); // when user added product is generated into the above line, now its time to make an array of object where each product object will be the element of an array. why do we needed to push the product objects into an array? because in every clieck the cart state changes, and en every change we need to pass usestate() a parametere, by default its parameter is set as an array so in every click of adding product we need to call the setCart() of useState and pass an array of product object so things get a real time update.
            }
         }
 
         // step 5: set the cart
-        setCart(savedCart);
+        setCart(savedCart); // in every click to add a new product setCart will be called and update its default value(which is an empty array in useState() function) with the newly added product. 
 
-    },[products])  // look back ref module 51, vedio 5 timestamp:08 min 18sec
+    },[products])  // look back ref module 51, vedio 5 timestamp:08 min 18sec: here we pushed products in the dependency array [products] so that everytime data fetching is delayed useEffect() may delay otherwise inside of this function will fall into error due to not finding products to do further operation. 
 
     const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
+        let newCart = [];
+        const exists = cart.find(pd => pd.id === product.id);
+
+        if(!exists){  // in case of adding new product into the cart
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }else{ // in case of adding product that already exists into the cart
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
         setCart(newCart);
         addToDb(product.id);
     }
